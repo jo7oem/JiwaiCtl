5b688b92d9ee4c7a2c79d68179ce5112051f5856
jo7oem <jo7oem@gmail.com>
2019-07-18 16:59:34 +0900

    Add レンジ指定

--- machines_controller/gauss_ctl.py
+++ machines_controller/gauss_ctl.py
@@ -3,12 +3,15 @@ import visa
 
 class GaussMeter:
     def __init__(self):
-        self._gs = visa.ResourceManager().open_resource("ASRL3::INSTR")  # linux "ASRL/dev/ttyUSB0::INSTR"
+        self.__gs = visa.ResourceManager().open_resource("ASRL3::INSTR")  # linux "ASRL/dev/ttyUSB0::INSTR"
 
-    def _query(self, command):
-        res = self._gs.query(command)
+    def __query(self, command: str) -> None:
+        res = self.__gs.query(command)
         res.translate(str.maketrans('', '', ' \r\n'))
         return res
+
+    def __write(self, command: str) -> None:
+        self.__gs.write(command)
 
     def magnetic_field_fetch(self) -> int:
         """磁界の値を測定機器に問い合わせ,1Gauss単位で返す
@@ -17,8 +20,8 @@ class GaussMeter:
         :rtype int
         """
 
-        res = self._query("FIELD?")
-        multiplier = self._query("FIELDM?")
+        res = self.__query("FIELD?")
+        multiplier = self.__query("FIELDM?")
         if multiplier == "m":
             res = res * 10 ** (-3)
         elif multiplier == "k":
@@ -29,5 +32,20 @@ class GaussMeter:
         return round(res)
 
     def set_query(self, command):
-        w = self._query(command)
+        w = self.__query(command)
         print(w)
+
+    def readable_magnetic_field_fetch(self) -> str:
+        """磁界の値を測定機器に問い合わせ,人間が読みやすい形で返す
+
+        :return: 磁界の値
+        :rtype str
+        """
+        field_str = self.__query("FIELD?") + self.__query("FIELDM?") + self.__query("UNIT?")
+        return field_str
+
+    def range_set(self, range_index: int) -> None:
+        if range_index < 0 or range_index > 3:
+            range_index = 0
+        self.__write("RANGE " + str(range_index))
+        return
