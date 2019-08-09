7e2177ca0f34ae03026ba4a3b0a549a7d642393c
jo7oem <jo7oem@gmail.com>
2019-08-09 18:01:55 +0900

    Add 設定値もログに吐くようにした

--- JiwaiCtl.py
+++ JiwaiCtl.py
@@ -179,25 +179,25 @@ def gen_csv_header(filename) -> datetime:
         writer.writerow(["開始時刻", start_time.strftime('%Y-%m-%d_%H-%M-%S')])
         writer.writerow(["memo", memo])
         writer.writerow(["#####"])
-        writer.writerow(["経過時間[sec]", "設定電流:ISET[A]", "出力電流:IOUT[A]", "磁界:H[Gauss]", "出力電圧:VOUT[V]", "IFINE"])
+        writer.writerow(["経過時間[sec]", "設定電流:ISET[A]", "出力電流:IOUT[A]", "磁界:H[Gauss]", "出力電圧:VOUT[V]", "設定値[G or I]"])
     return start_time
 
 
-def save_status(filename: str, status: StatusList) -> None:
+def save_status(filename: str, status: StatusList, target=0.0) -> None:
     """
     ファイルにステータスを追記する
 
     --------
     :type status: dict{"iset":float,"iout":float,"ifield"}
     :param filename: 書き込むファイル名
-    :param status: 書き込むデー   タ
+    :param status: 書き込むデータ
     :return:
     """
     result = status.out_tuple()
 
     with open(filename, mode='a', encoding="utf-8")as f:
         writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(result)
+        writer.writerow(result, target)
     return
 
 
@@ -217,7 +217,7 @@ def mesure_process(mesure_setting, mesure_seq, start_time, save_file=None):
         status.set_origine_time(start_time)
         print(status)
         if save_file:
-            save_status(save_file, status)
+            save_status(save_file, status, target=target)
         time.sleep(post_lock_time)
     return
 
