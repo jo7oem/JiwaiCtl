23024565a12ec3e79a1027bc0b5168e8610bf85f
jo7oem <jo7oem@gmail.com>
2020-10-29 15:02:32 +0900

    fix JSONファイルLoadに関する異常時動作の間違いを修正

--- machines_controller/gauss_ctl.py
+++ machines_controller/gauss_ctl.py
@@ -41,32 +41,30 @@ class GaussMeter:
             pass
         return res
 
+    def readable_magnetic_field_fetch(self) -> str:
+        """磁界の値を測定機器に問い合わせ,人間が読みやすい形で返す
 
-def readable_magnetic_field_fetch(self) -> str:
-    """磁界の値を測定機器に問い合わせ,人間が読みやすい形で返す
+        :return: 磁界の値
+        :rtype str
+        """
+        field_str = self.__query("FIELD?") + self.__query("FIELDM?") + self.__query("UNIT?")
+        return field_str
 
-    :return: 磁界の値
-    :rtype str
-    """
-    field_str = self.__query("FIELD?") + self.__query("FIELDM?") + self.__query("UNIT?")
-    return field_str
-
-
-def range_set(self, range_index: int) -> None:
-    """
-    レンジを切り替える
-    0:~30.00 kOe
-    1:~3.000 kOe
-    2:~300.0 Oe
-    3:~30.00 Oe
-    :param range_index:
-    :return:
-    """
-    if range_index < 0 or range_index > 3:
-        range_index = 0
-    self.__write("RANGE " + str(range_index))
-    time.sleep(0.2)
-    return
+    def range_set(self, range_index: int) -> None:
+        """
+        レンジを切り替える
+        0:~30.00 kOe
+        1:~3.000 kOe
+        2:~300.0 Oe
+        3:~30.00 Oe
+        :param range_index:
+        :return:
+        """
+        if range_index < 0 or range_index > 3:
+            range_index = 0
+        self.__write("RANGE " + str(range_index))
+        time.sleep(0.2)
+        return
 
 
 def range_fetch(self) -> int:
